8c6922d01e9b825f32ac5c7806ca72221c6faf80
Egor Fedorov <fedorovegor33@gmail.com>
2015-04-22 15:50:51 +0300

    Changed dependecies

--- dependencies.sh
+++ dependencies.sh
@@ -11,11 +11,10 @@ apt-get install -y python-pip
 apt-get install -y libjpeg zlib postgresql libmysqlclient
 apt-get install -y libjpeg-dev libfreetype6-dev zlib1g-dev
 easy_install -U pip
-pip install djangocms-installer
-pip install Django
-pip install --upgrade --force-reinstall pillow
-pip install django-registration-redux
+pip install requests[security]
+pip install Django==1.7.4
 pip install Pinax
+pip install -r prj/XS/requirements.txt
 
 if ! [ -L /var/www ]; then
       rm -rf /var/www
